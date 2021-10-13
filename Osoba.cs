9e4307ccfa250fad7346d7a85501a5a223dfe1f9
plextor15 <75131243+plextor15@users.noreply.github.com>
2021-10-13 13:47:21 +0200

    List, HashSet, Dictionary

---
+++ Osoba.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PierwszyASP
+{
+    class Osoba
+    {
+        public string Imie;
+        public string Nazwisko;
+
+        public string DataUr { private set; get; }
+        public long Pesel { private set; get; }
+
+        public Osoba(string imie, string nazwisko, string dataUr, long pesel)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            DataUr = dataUr;
+            Pesel = pesel;
+        }
+
+        public override string ToString()
+        {
+            return Imie + " " + Nazwisko + ", PESEL: " + Pesel;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Osoba osoba &&
+                   Pesel == osoba.Pesel;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Pesel);
+        }
+
+        // Tak nie mozna robic, bo sie wszystko rozleci
+        //Random rand = new Random();
+        //public override int GetHashCode()
+        //{
+        //    return rand.Next();
+        //}
+    }
+}
